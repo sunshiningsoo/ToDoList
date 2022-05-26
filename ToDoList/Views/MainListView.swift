7b785c2b2f061e474c5e1eba890bcc9f45155714
sunshiningsoo <yenkee1219@gmail.com>
2022-05-26 23:10:16 +0900

    MVVM 구조 나누기

---
+++ ToDoList/Views/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  ToDoList
+//
+//  Created by 박성수 on 2022/05/26.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
